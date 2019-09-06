8376bc1e7eaea77cd8fd589eda39722afaf55a6c
Klim Naydenov <knaydenov@gmail.com>
2019-09-06 21:13:55 +0300

    Add tests

---
+++ Tests/Plugin/YandexCheckoutPluginTest.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace Kna\PaymentYandexCheckoutBundle\Tests\Plugin;
+
+
+use PHPUnit\Framework\TestCase;
+
+class YandexCheckoutPluginTest extends TestCase
+{
+    // TODO: add tests
+}
